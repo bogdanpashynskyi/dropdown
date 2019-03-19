ea2ed8377d8cdb11a7da930bcff237150644d584
Bogdan Pashynskyi <bogdanpashynskyi@Bogdans-MacBook-Pro.local>
2019-03-19 22:13:01 +0200

    working version without styles

--- src/App.js
+++ src/App.js
@@ -3,11 +3,48 @@ import React, { Component } from 'react';
 import './App.css';
 import Dropdown from './Dropdown';
 
+const config = [
+  {id: 1, value: "dropdown", text: "Dropdown"},
+  {id: 2, value: "preview", text: "Preview"},
+  {id: 3, value: "rename", text: "Rename"},
+  {id: 4, value: "delete", text: "Delete"},
+  {id: 5, value: "separate-link", text: "Separate link"},
+]
+
 class App extends Component {
+  state = {
+    options: config,
+    isOpen: false,
+    selected: config[0],
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen,
+    }))
+  }
+
+  handleSelected = (id) => {
+    this.setState(prevState => (
+      {
+        selected: prevState.options.find(item => item.id === id),
+        isOpen: false,
+      }
+    ))
+  }
+
   render() {
+    const { options, isOpen, selected } = this.state;
+    console.log(selected);
     return (
       <div className="App">
-        <Dropdown/>
+        <Dropdown
+        options={options}
+        isOpen={isOpen}
+        selected={selected}
+        handleClick={this.handleClick}
+        handleSelected={this.handleSelected}
+        />
       </div>
     );
   }
--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,7 +1,31 @@
-import React from 'React';
+import React from 'react';
 
-const Dropdown = () => {
+const Dropdown = (props) => {
+  const {handleClick, handleSelected, options, selected, isOpen} = props;
+
+  const itemsForSelection = options.filter(option => 
+    option.id !== selected.id)
+
   return (
-    <div> I am a dropdown menu </div>
+    <div> 
+        <div 
+        className="Dropdown__selector"
+        onClick={() => handleClick()}
+        >
+        {selected.text}
+        </div>
+
+        {itemsForSelection.map(item => 
+          <div
+          key={item.id}
+          onClick={() => handleSelected(item.id)}
+          className={"Dropdown__selector-items " + (isOpen ? 'open' : 'closed')}
+          >
+          {item.text}
+          </div>
+        )}
+    </div>
   )
 }
+
+export default Dropdown;
